7a50d39f7d9e4901174306a4b5f9c2be8a31d6bf
AKBarcenas <akbarcen@ucsd.edu>
2015-12-30 02:16:11 -0800

    Modified the storyboad and created outlets

--- Swifty Words/ViewController.swift
+++ Swifty Words/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func submitTapped(sender: UIButton) {
+    }
 
+    @IBAction func clearTapped(sender: UIButton) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

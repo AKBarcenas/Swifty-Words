6075f180ee1f45f4e085e2410ac0e5b321eac947
AKBarcenas <akbarcen@ucsd.edu>
2015-12-31 00:39:47 -0800

    Added functionality to buttons

--- Swifty Words/ViewController.swift
+++ Swifty Words/ViewController.swift
@@ -35,9 +35,43 @@ class ViewController: UIViewController {
     }
     
     @IBAction func submitTapped(sender: UIButton) {
+        if let solutionPosition = solutions.indexOf(currentAnswer.text!) {
+            activatedButtons.removeAll()
+            
+            var splitClues = answersLabel.text!.componentsSeparatedByString("\n")
+            splitClues[solutionPosition] = currentAnswer.text!
+            answersLabel.text = splitClues.joinWithSeparator("\n")
+            
+            currentAnswer.text = ""
+            ++score
+            
+            if score % 7 == 0 {
+                let ac = UIAlertController(title: "Well done!", message: "Are you ready for the next level?", preferredStyle: .Alert)
+                ac.addAction(UIAlertAction(title: "Let's go!", style: .Default, handler: levelUp))
+                presentViewController(ac, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    func levelUp(action: UIAlertAction!) {
+        ++level
+        solutions.removeAll(keepCapacity: true)
+        
+        loadLevel()
+        
+        for btn in letterButtons {
+            btn.hidden = false
+        }
     }
 
     @IBAction func clearTapped(sender: UIButton) {
+        currentAnswer.text = ""
+        
+        for btn in activatedButtons {
+            btn.hidden = false
+        }
+        
+        activatedButtons.removeAll()
     }
     
     override func didReceiveMemoryWarning() {
@@ -83,6 +117,12 @@ class ViewController: UIViewController {
                 letterButtons[i].setTitle(letterBits[i], forState: .Normal)
             }
         }
+    }
+    
+    func letterTapped(btn: UIButton) {
+        currentAnswer.text = currentAnswer.text! + btn.titleLabel!.text!
+        activatedButtons.append(btn)
+        btn.hidden = true
     }
 
 }
